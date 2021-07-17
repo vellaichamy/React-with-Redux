a02ac419481afa3248c2a86bd4607242f35a1d7c
vellaichamy <uvellaichamy@gmail.com>
2021-07-18 00:00:41 +0530

    1 file modified, 2 added

--- src/03/CommunicatingWithProps.js
+++ src/03/CommunicatingWithProps.js
@@ -1,17 +1,46 @@
 import React from 'react';
-
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const CommunicatingWithProps = () => {
   return (
     <>
-      <CommentDetail author="Rithu" timeAgo="Today at 4:45PM"  content="I like the subject" />
-      <CommentDetail author="Adithya" timeAgo="Today at 4:45PM"  content="I like the subject" />
-      <CommentDetail
-        author="Sankar"
-        timeAgo="Today at 4:45PM"
-        content="I like the subject"
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Rithu"
+          timeAgo="Today at 4:45PM"
+          content="I like the subject"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Adithya"
+          timeAgo="Today at 4:45PM"
+          content="I like the subject"
+        />
+      </ApprovalCard>{' '}
+      <ApprovalCard>
+        <CommentDetail
+          author="Sankar"
+          timeAgo="Today at 4:45PM"
+          content="I like the subject"
+        />
+      </  ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </>
   );
 };
