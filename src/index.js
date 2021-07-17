3849578c6716d7cd2e273b1c8395e562aad55521
vellaichamy <uvellaichamy@gmail.com>
2021-07-17 20:12:17 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Converting from './Converting';
+// import Converting from './Converting';
 // import Converting from "./App";
+import Converting from "./02/Converting";
 
 ReactDOM.render(<Converting />, document.getElementById('root'));
