08c376d7bf4f155b2dd51838c1f3e842a7bf6739
vellaichamy <uvellaichamy@gmail.com>
2021-07-17 22:59:19 +0530

    1 file modified, 1 added

--- src/03/CommunicatingWithProps.js
+++ src/03/CommunicatingWithProps.js
@@ -1,52 +1,18 @@
 import React from 'react';
-import faker from 'faker';
+
+import CommentDetail from './CommentDetail';
 
 const CommunicatingWithProps = () => {
   return (
-    <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-    </div>
+    <>
+      <CommentDetail author="Rithu" timeAgo="Today at 4:45PM"  content="I like the subject" />
+      <CommentDetail author="Adithya" timeAgo="Today at 4:45PM"  content="I like the subject" />
+      <CommentDetail
+        author="Sankar"
+        timeAgo="Today at 4:45PM"
+        content="I like the subject"
+      />
+    </>
   );
 };
 
