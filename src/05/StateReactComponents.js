88b1bedaf82b1daa9db3c3c93d3104609b6c099b
vellaichamy <uvellaichamy@gmail.com>
2021-07-18 17:07:34 +0530

    2 files modified, 1 added

---
+++ src/05/StateReactComponents.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+class StateReactComponents extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // we called setstate!!!!
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
+  }
+}
+
+// ReactDOM.render(<App />, document.querySelector("#root"));
--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Converting from "./03/CommunicatingWithProps";
+// import Converting from './05/StateReactComponents';
+// import Converting from "./03/CommunicatingWithProps";
 // import Converting from "./02/Converting";
 // import Converting from './Converting';
 // import Converting from "./App";
 
+if (module.hot) {
+  module.hot.accept();
+}
+
+class Converting extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // we called setstate!!!!
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
+  }
+}
 
 ReactDOM.render(<Converting />, document.getElementById('root'));
