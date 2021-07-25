ae358060a0d1eb8564ab363f49b545753e53fa6b
vellaichamy <uvellaichamy@gmail.com>
2021-07-25 12:23:43 +0530

    1 file modified, 6 added

---
+++ src/10/components/ImageCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+  render() {
+    const { description, urls } = this.props.image;
+
+    return (
+      <div>
+        <img alt={description} src={urls.regular} />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
