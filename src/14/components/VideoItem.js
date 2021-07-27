bd41cdf892e19b8776c46a95ed111e604fbf6186
vellaichamy <uvellaichamy@gmail.com>
2021-07-27 22:32:52 +0530

    4 files modified

--- src/14/components/VideoItem.js
+++ src/14/components/VideoItem.js
@@ -9,9 +9,9 @@ const VideoItem = ({ video, onVideoSelect }) => {
         className="ui image"
         src={video.snippet.thumbnails.medium.url}
       />
-      {/* <div className="content">
+      <div className="content">
         <div className="header">{video.snippet.title}</div>
-      </div> */}
+      </div>
     </div>
   );
 };
