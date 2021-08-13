e277d136b2391993a8651a0f2d94c96741c13fc1
vellaichamy <uvellaichamy@gmail.com>
2021-08-13 22:49:10 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
 // import App from './components/App'
 // import rootReducer from './reducers'
 
