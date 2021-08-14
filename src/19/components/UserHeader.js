8d7b1e053a824af50eef947831341612f2696209
vellaichamy <uvellaichamy@gmail.com>
2021-08-15 00:07:56 +0530

    2 files modified

--- src/19/components/UserHeader.js
+++ src/19/components/UserHeader.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+
   render() {
-    const { user } = this.props;
-
-    if (!user) {
-      return null;
-    }
-
-    return <div className="header">{user.name}</div>;
+    return <div>User Header {this.props.userId}</div>;
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  return { user: state.users.find(user => user.id === ownProps.userId) };
-};
-
-export default connect(mapStateToProps)(UserHeader);
+export default connect(
+  null,
+  { fetchUser }
+)(UserHeader);
